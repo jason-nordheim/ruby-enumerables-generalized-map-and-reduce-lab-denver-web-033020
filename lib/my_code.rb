b7f765e48cb54c5189c2129dc0fee17bbac2f768
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-12 16:19:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,18 +1,18 @@
 # Your Code Here
-# input: 
-# => array of data 
-# => block of code to be executed on the array 
-# output: 
-# => array with each item run against the code 
-map(data) do | block | 
-  yield block  
-end 
+# input:
+# => array of data
+# => block of code to be executed on the array
+# output:
+# => array with each item run against the code
+map(data) do | block |
+  yield block
+end
 
-# input: 
-# => array of data 
-# => block of code to be executed on the array 
-# output: 
+# input:
+# => array of data
+# => block of code to be executed on the array
+# output:
 # => a true or false
-reduce(data) do | block | 
+reduce(data) do | block |
   yield block
 end 
